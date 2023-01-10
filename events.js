5d51ab674c483255898eda05b478e0c13d06eef0
Brian Conley <78391712+OG-Vintage1000@users.noreply.github.com>
2023-01-10 16:21:19 -0600

    Updating code for exercise

--- events.js
+++ events.js
@@ -25,10 +25,24 @@ function fizzbuzz(){
     console.log(document.querySelector('#fizzbuzz-input').value);
     //you'll want to look up
     //javascript loops (either while, or for will work well)
+    let n;
+    for (let i = 0; i < n; i++) {
+        if(i % 3 == 0 ){
+            console.log("Fizz"+(i));
+        // } else if(i % 5 == 0){
+        //     console.log("Buzz"+(i));
+        // } else if(i % 15 == 0){
+        //     console.log("FizzBuzz"+(i));
+        } else {
+            console.log(i);
+        }
+        
+    }
     //conditionals(if, elseif, else) or switch statements
     //modulo operator will also come handy here
+    console.log(document.querySelector('#fizzbuzz-output').value);
 }
 
 function calculator(value){
-
+    const calcValue = document.getElementById("expression")
 }
