0fdc098564b22e1b87d0b1ce256da1156a91c2cf
Brian Conley <78391712+OG-Vintage1000@users.noreply.github.com>
2023-01-10 15:28:16 -0600

    Session Ex and Activities

---
+++ events.js
@@ -0,0 +1,34 @@
+let num = 1;
+function increment(){
+    const countElem = document.getElementById("count");
+    countElem.innerText = ++num;
+}
+
+function decrement(){
+    const countElem = document.getElementById("count");
+    countElem.innerText = --num;
+}   
+
+function reset(){
+    const countElem = document.getElementById("count");
+    num = 0;
+    countElem.innerText = 0;
+}
+
+function modifyNum(number){
+    const countElem = document.getElementById("count");
+    num += number;
+    countElem.innerText = num;
+}
+
+function fizzbuzz(){
+    console.log(document.querySelector('#fizzbuzz-input').value);
+    //you'll want to look up
+    //javascript loops (either while, or for will work well)
+    //conditionals(if, elseif, else) or switch statements
+    //modulo operator will also come handy here
+}
+
+function calculator(value){
+
+}
