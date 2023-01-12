d7e0245b055abc69d9e2337da1e858b1d6d23a2b
Brian Conley <78391712+OG-Vintage1000@users.noreply.github.com>
2023-01-12 17:17:40 -0600

    Fetch changed
    - Trying out fetch example
    - Add more features in fetch.html

--- fetch.js
+++ fetch.js
@@ -14,3 +14,21 @@
 // })
 
 // **/apiKey.jason  -- Look for apiKey with extension and ignore; will gray it out
+
+//fetch('http://example.com/movies.json').then((response) => response.json()).then((data) => console.log(data));
+
+//Use http.garden as next example
+
+function fetch(){
+
+}
+
+function dummy() {
+    const image = document.querySelector("my-image");
+    fetch('flower.jpg')
+        .then((response) => response.blob())
+        .then((blob) => {
+            const objectURL = URL.createObjectURL(blob);
+            image.src = objectURL;
+        });
+}
