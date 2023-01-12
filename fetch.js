f0ca8c0794b7760f347888c3ed34d8eff2d26569
Brian Conley <78391712+OG-Vintage1000@users.noreply.github.com>
2023-01-12 14:42:37 -0600

    Fetch
    - Created new html and js

---
+++ fetch.js
@@ -0,0 +1,16 @@
+// Javascript offers two built in ways to send http requests
+// - XML HTTP Request
+// - Fetch API
+
+// function fetch(){
+//     const inputVal = document.getElementById('status-code').ariaValueMax;
+
+//     document.getElementById('status-code').src = `https://http.cat/${inputVal}.jpg`
+// }
+
+// fetch('.apiKey.json').then((res) => res.json()).then((data) =>
+// {
+//     apiKey = data.TheCatAPI
+// })
+
+// **/apiKey.jason  -- Look for apiKey with extension and ignore; will gray it out
